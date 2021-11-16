c5c8444dbd46b9871f80e69995d7771ab87b035d
Maxim Wolotschij <m.wolotschij@web.de>
2021-11-16 14:57:16 +0100

    add index endpoint

--- booklist/server.rb
+++ booklist/server.rb
@@ -20,6 +20,28 @@ class Book
 
   index({ title: 'text' })
   index({ isbn:1 }, { unique: true, name: "isbn_index" })
+
+  scope :title, -> (title) { where(title: /^#{title}/) }
+  scope :isbn, -> (isbn) { where(isbn: isbn) }
+  scope :author, -> (author) { where(author: author) }
+end
+
+# Serializers to normalize JSON output
+class BookSerializer
+  def initialize(book)
+    @book = book
+  end
+
+  def as_json(*)
+    data = {
+      id:@book.id.to_s,
+      title:@book.title,
+      author:@book.author,
+      isbn:@book.isbn
+    }
+    data[:errors] = @book.errors if@book.errors.any?
+    data
+  end
 end
 
 # Endpoints
@@ -27,6 +49,22 @@ get '/ ' do
   'Welcome to BookList!'
 end
 
+# Useful for versioning.
 namespace '/api/v1' do
+
+  before do
+    content_type 'application/json'
+  end
+
+  get '/books' do  #=>  .../api/v1/books
+    books = Book.all
+
+      # Go through each defined scope and filter the books if a value was given for this specific scope.
+      [:title, :isbn, :author].each do |filter|
+        books = books.send(filter, params[filter]) if params[filter]
+      end
+
+    books.map { |book| BookSerializer.new(book) }.to_json  # Instead of books.to_json to normalize output.
+  end
 
 end
